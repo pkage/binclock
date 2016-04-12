a1d81ea3074174d6ffdfcfd26d97a9771532e7b2
Patrick Kage <quadnix1@gmail.com>
2016-04-12 16:42:59 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+convertToBitfield = function(num) {
+	var field = {};
+
+	while (num != 0) {
+		var n;
+		for (n = 0; num - Math.pow(2, n + 1) >= 0; n++) {
+			field[Math.pow(2, n)] = false;
+		}
+		field[Math.pow(2, n )] = true;
+		num -= Math.pow(2, n );
+	}
+	return field;
+
+}
+
+updateRow = function(limiter, number) {
+	var field = convertToBitfield(number);
+	for (var property in field) {
+		if (field.hasOwnProperty(property)) {
+			var sel = $('[data-role=' + limiter + '] > [data-value=' + property + ']');
+			if (field[property]) {
+				sel.addClass('on');
+			} else {
+				sel.removeClass('on');
+			}
+		}
+	}
+}
+
+updateClock = function() {
+	var date = new Date();
+	updateRow('hours', date.getHours());
+	updateRow('minutes', date.getMinutes());
+	updateRow('seconds', date.getSeconds());
+}
+
+$(document).ready(function() {
+	updateClock();
+	setInterval(updateClock, 500);
+})
